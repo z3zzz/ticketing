0a48ef9f6b075b69803f0a0843188b7d9306f6b0
Kwangtae Kim <zabra7946@naver.com>
2022-08-05 16:22:42 +0900

    fix env variable

--- web/utils/constants.ts
+++ web/utils/constants.ts
@@ -1,2 +1,3 @@
-export const AUTH_API_HOST = process.env.AUTH_API_HOST || 'localhost:5000';
+export const AUTH_API_HOST =
+  process.env.NEXT_PUBLIC_AUTH_API_HOST || 'localhost:5000';
 export const NODE_ENV = process.env.NEXT_PUBLIC_NODE_ENV || 'development';
