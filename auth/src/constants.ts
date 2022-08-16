af223dea3c35a32ae85a90f6c03d1cb72e73b144
Kwangtae Kim <zabra7946@naver.com>
2022-08-16 16:35:03 +0900

    change default db name

--- auth/src/constants.ts
+++ auth/src/constants.ts
@@ -1,7 +1,6 @@
 export const PORT = parseInt(process.env.PORT || '5000');
 export const POSTGRES_URL =
-  process.env.POSTGRES_URL ||
-  'postgresql://postgres:1234@localhost:5432/instagram';
+  process.env.POSTGRES_URL || 'postgresql://postgres:1234@localhost:5432/auth';
 export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 export const COOKIE_KEY = process.env.COOKIE_KEY;
 export const DOMAIN = process.env.DOMAIN || 'localhost';
